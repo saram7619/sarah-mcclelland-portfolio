1c35766a7df215c5b6b2056827d223d72d39a7cd
Sarah McClelland <82645022+saram7619@users.noreply.github.com>
2023-06-14 13:10:45 +0100

    Update Projects.tsx

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -86,12 +86,12 @@ const Projects = () => {
               <div className='flex items-center space-x-6 my-6 lg:hidden'>
                 <Link
                   className='text-white text-base underline-offset-8 underline decoration-fuchsia-700 hover:text-fuchsia-700'
-                  href={project.url}>
+                  href={`/${project.url}`}>
                   VIEW PROJECT
                 </Link>
                 <Link
                   className='text-white text-base underline-offset-8 underline decoration-fuchsia-700 hover:text-fuchsia-700'
-                  href={project.repo}>
+                  href={`/${project.repo}`}
                   VIEW CODE
                 </Link>
               </div>
